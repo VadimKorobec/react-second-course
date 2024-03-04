8f37c8a86ddd51ae024fb185074791b656a086e7
VadimKorobec <_s_e_t__@ukr.net>
2024-03-04 16:38:09 +0200

    add Modal

--- src/components/Todo/Todo.jsx
+++ src/components/Todo/Todo.jsx
@@ -1,10 +1,29 @@
-export const Todo = () => {
+import { useState } from "react";
+
+import { Modal } from "../Modal/Modal";
+import { Backdrop } from "../Backdrop/Backdrop";
+
+export const Todo = (props) => {
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+
+  const deleteHandler = () => {
+    setModalIsOpen(true);
+  };
+
+  const closeModalHandler = () => {
+    setModalIsOpen(false);
+  };
+
   return (
     <div className="card">
-      <h2>TITLE</h2>
+      <h2>{props.text}</h2>
       <div className="actions">
-        <button className="btn">Delete</button>
+        <button className="btn" onClick={deleteHandler}>
+          Delete
+        </button>
       </div>
+      {modalIsOpen && <Modal />}
+      {modalIsOpen && <Backdrop onClick={closeModalHandler} />}
     </div>
   );
 };
