f27a855bcbd38c8f24a2f6f3d50f522bde626eb8
VadimKorobec <_s_e_t__@ukr.net>
2024-03-06 20:29:26 +0200

    add useEffect

--- src/pages/AllMeetups.jsx
+++ src/pages/AllMeetups.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { MeetupList } from "../components/Meetups/MeetupList";
 
 const DUMMY_DATA = [
@@ -22,11 +23,45 @@ const DUMMY_DATA = [
 ];
 
 export const AllMeetupsPage = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [meetups, setMeetups] = useState([]);
+  console.log(meetups);
+
+  useEffect(() => {
+    setIsLoading(true);
+    fetch(
+      "https://fresh-circle-397613-default-rtdb.firebaseio.com/meetups.json"
+    )
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        const meetups = [];
+        for (const key in data) {
+          const meetup = {
+            id: key,
+            ...data[key],
+          };
+          meetups.push(meetup);
+        }
+        setIsLoading(false);
+        setMeetups(meetups);
+      });
+  }, []);
+
+  if (isLoading) {
+    return (
+      <section>
+        <p>Loading ...</p>
+      </section>
+    );
+  }
+
   return (
     <section>
       <h1 style={{ textAlign: "center" }}>All Meetups</h1>
       <ul>
-        <MeetupList meetups={DUMMY_DATA} />
+        <MeetupList meetups={meetups} />
       </ul>
     </section>
   );
--- src/pages/NewMeetup.jsx
+++ src/pages/NewMeetup.jsx
@@ -1,6 +1,9 @@
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 import { NewMeetupForm } from "../components/Meetups/NewMeetupForm";
 
 export const NewMeetupPage = () => {
+  const history = useHistory();
+
   const handleAddNewMeetup = (meetupData) => {
     fetch(
       "https://fresh-circle-397613-default-rtdb.firebaseio.com/meetups.json",
@@ -11,7 +14,9 @@ export const NewMeetupPage = () => {
           "Content-Type": "application/json",
         },
       }
-    );
+    ).then(() => {
+      history.replace("/");
+    });
   };
 
   return (
