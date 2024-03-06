1eae670e734952fccfb6e5da449b98066488c60f
VadimKorobec <_s_e_t__@ukr.net>
2024-03-06 19:16:32 +0200

    add fetch

--- src/components/Meetups/NewMeetupForm.jsx
+++ src/components/Meetups/NewMeetupForm.jsx
@@ -1,26 +1,101 @@
+import { useRef } from "react";
+
 import { Card } from "../ui/Card";
-
 import styles from "./NewMeetupForm.module.css";
+import { useState } from "react";
 
-export const NewMeetupForm = () => {
+export const NewMeetupForm = (props) => {
+  const [title, setTitle] = useState("");
+  const [image, setImage] = useState("");
+  const [address, setAddress] = useState("");
+  const [description, setDescription] = useState("");
+
+  const handleTitleChange = (e) => {
+    setTitle(e.target.value);
+  };
+  const handleImageChange = (e) => {
+    setImage(e.target.value);
+  };
+  const handleAddressChange = (e) => {
+    setAddress(e.target.value);
+  };
+
+  const handleDescriptionChange = (e) => {
+    setDescription(e.target.value);
+  };
+
+  const titleInputRef = useRef();
+  const imageInputRef = useRef();
+  const addressInputRef = useRef();
+  const descriptionInputRef = useRef();
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+    const enteredTitle = titleInputRef.current.value;
+    const enteredImage = imageInputRef.current.value;
+    const enteredAddress = addressInputRef.current.value;
+    const enteredDescription = descriptionInputRef.current.value;
+
+    const meetupData = {
+      title: enteredTitle,
+      image: enteredImage,
+      address: enteredAddress,
+      description: enteredDescription,
+    };
+
+    const meetupData2 = {
+      title: title,
+      image: image,
+      address: address,
+      description: description,
+    };
+
+    props.onAddMeetup(meetupData);
+  };
+
   return (
     <Card>
-      <form className={styles.form}>
+      <form className={styles.form} onSubmit={submitHandler}>
         <div className={styles.control}>
           <label htmlFor="title">Meetup Title</label>
-          <input type="text" required id="title" />
+          <input
+            type="text"
+            required
+            id="title"
+            ref={titleInputRef}
+            onChange={handleTitleChange}
+          />
         </div>
         <div className={styles.control}>
-          <label htmlFor="image">Meetup Title</label>
-          <input type="url" required id="image" />
+          <label htmlFor="image">Meetup Image</label>
+          <input
+            type="url"
+            required
+            id="image"
+            ref={imageInputRef}
+            onChange={handleImageChange}
+          />
         </div>
         <div className={styles.control}>
           <label htmlFor="address">Address</label>
-          <input type="text" required id="address" />
+          <input
+            type="text"
+            required
+            id="address"
+            ref={addressInputRef}
+            onChange={handleAddressChange}
+          />
         </div>
         <div className={styles.control}>
           <label htmlFor="description">Address</label>
-          <textarea id="description" required rows="5"></textarea>
+          <textarea
+            id="description"
+            required
+            rows="5"
+            ref={descriptionInputRef}
+            onChange={handleDescriptionChange}
+          ></textarea>
         </div>
         <div className={styles.actions}>
           <button type="submit">Add Meetup</button>
--- src/pages/NewMeetup.jsx
+++ src/pages/NewMeetup.jsx
@@ -1,10 +1,23 @@
 import { NewMeetupForm } from "../components/Meetups/NewMeetupForm";
 
 export const NewMeetupPage = () => {
+  const handleAddNewMeetup = (meetupData) => {
+    fetch(
+      "https://fresh-circle-397613-default-rtdb.firebaseio.com/meetups.json",
+      {
+        method: "POST",
+        body: JSON.stringify(meetupData),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+  };
+
   return (
     <section>
       <h1 style={{ textAlign: "center" }}>Add New Meetup</h1>
-      <NewMeetupForm />
+      <NewMeetupForm onAddMeetup={handleAddNewMeetup} />
     </section>
   );
 };
